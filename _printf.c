59567f5a36e14a274e91272836318f72427d2fcc
khadija qoudmanne <rizkilive@gmail.com>
2023-04-22 14:58:38 -0700

    team project

--- _printf.c
+++ _printf.c
@@ -13,12 +13,9 @@ int i, printed = 0, printed_chars = 0;
 int flags, width, precision, size, buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
-
 if (format == NULL)
 return (-1);
-
 va_start(list, format);
-
 for (i = 0; format && format[i] != '\0'; i++)
 {
 if (format[i] != '%')
@@ -44,11 +41,8 @@ return (-1);
 printed_chars += printed;
 }
 }
-
 print_buffer(buffer, &buff_ind);
-
 va_end(list);
-
 return (printed_chars);
 }
 
@@ -61,6 +55,5 @@ void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
-
 *buff_ind = 0;
 }
